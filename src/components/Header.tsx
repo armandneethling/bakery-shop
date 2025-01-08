c8e8defce4221e9ad4d043a951864704b2fe3db4
Armand Neethling <20231315@ctucareer.co.za>
2025-01-08 16:14:30 +0200

    new file:   favicon.ico 	modified:   index.html 	deleted:    logo.png 	modified:   src/components/Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import logo from '/logo.png';
+import logo from '../assets/images/header-logo.png';
 
 const Header = () => (
     <header className="header-container">
