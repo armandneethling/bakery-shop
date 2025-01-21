0ea67d306b36fbbf94a8fd25a6aff9ac1be4ec08
SlothyKoala <aneethling21@gmail.com>
2025-01-21 15:54:31 +0200

    modified:   src/components/AboutUs.tsx 	modified:   src/components/GallerySlideshow.tsx 	modified:   src/styles/tailwind.css

--- src/components/GallerySlideshow.tsx
+++ src/components/GallerySlideshow.tsx
@@ -38,7 +38,23 @@ const GallerySlideshow = () => {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     };
 
     return (
