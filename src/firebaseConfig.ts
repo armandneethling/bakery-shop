acf18ffaf21fefd3c9ae54b7adfd764d00d9b400
SlothyKoala <aneethling21@gmail.com>
2025-02-05 20:36:16 +0200

    modified:   src/firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,7 +4,7 @@ import { getDatabase } from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzG0iEg_ldZIwqSp4pp_6Q_MG2Vyr5ZbY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
   authDomain: "bakerymanager-40c9e.firebaseapp.com",
   databaseURL: "https://bakerymanager-40c9e-default-rtdb.europe-west1.firebasedatabase.app/",
   projectId: "bakerymanager-40c9e",
