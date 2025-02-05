d46c036171285883bc943c5f2ea58b4989f481fa
SlothyKoala <aneethling21@gmail.com>
2025-02-05 20:53:58 +0200

    modified:   src/firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
-  authDomain: "bakerymanager-40c9e.firebaseapp.com",
-  databaseURL: "https://bakerymanager-40c9e-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "bakerymanager-40c9e",
-  storageBucket: "bakerymanager-40c9e.appspot.com",
-  messagingSenderId: "671517362545",
-  appId: "1:671517362545:web:8a9f34702af9a0081c4ed0",
-  measurementId: "G-X5ENTF6NZJ"
+  authDomain: 'bakerymanager-40c9e.firebaseapp.com',
+  databaseURL: 'https://bakerymanager-40c9e-default-rtdb.europe-west1.firebasedatabase.app/',
+  projectId: 'bakerymanager-40c9e',
+  storageBucket: 'bakerymanager-40c9e.appspot.com',
+  messagingSenderId: '671517362545',
+  appId: '1:671517362545:web:8a9f34702af9a0081c4ed0',
+  measurementId: 'G-X5ENTF6NZJ'
 };
 
 const app = initializeApp(firebaseConfig);
